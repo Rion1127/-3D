4079b35457f6a4fcee4a1e3f490bce70bed9faed
rion suzuki <rion2002m@gmail.com>
2023-06-12 22:51:59 +0900

    includeをまとめる用のファイルを作った

---
+++ engine/REngine.h
@@ -0,0 +1,20 @@
+#pragma once
+//é©çÏ
+#include "Vector2.h"
+#include "Vector3.h"
+#include "DirectX.h"
+
+#include <DirectXMath.h>
+#include <d3d12.h>
+#include <d3dcompiler.h>
+#include <d3dx12.h>
+#pragma comment(lib, "d3dcompiler.lib")
+#include <wrl.h>
+
+//C++
+#include <memory>
+#include <vector>
+#include <string>
+#include <list>
+#include <array>
+#include <map>
